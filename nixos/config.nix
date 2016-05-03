f90ff440ec58b4881c586b6e4aabd70d2e685b22
Tomas Vestelind <tomas.vestelind@gmail.com>
2016-05-03 16:44:16 +0200

    nixos config

---
+++ nixos/config.nix
@@ -0,0 +1,37 @@
+{
+  allowUnfree = true;
+
+  firefox.enableAdobeFlash = true;
+
+  packageOverrides = pkgs_: with pkgs_; {
+    all = with pkgs; buildEnv {
+      name = "all";
+      paths = [
+        autoconf
+        automake
+        evince
+        firefoxWrapper
+        gcc
+        git
+        gnupg
+        haskellPackages.cabal-install
+        haskellPackages.ghc
+        haskellPackages.stack
+        irssi
+        liberation_ttf
+        libreoffice
+        mplayer
+        nix-repl
+        pidgin
+        python27Packages.livestreamer
+        python3
+        python34Packages.nose
+        thunderbird
+        unzip
+        vim
+        vlc
+        zsh
+      ];
+    };
+  };
+}
